7c44dc2841cafb89124c823cc4ac47a0d765dbde
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-05-07 23:18:45 +0200

    start work on Home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,20 @@
 // Styles
 import './Home.scss';
+import Mountain from '../../assets/images/Mountain.png';
+import Swamp from '../../assets/images/Swamp.png';
+import Forest from '../../assets/images/Forest.png';
+import Island from '../../assets/images/Island.png';
+import Plains from '../../assets/images/Plains.png';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>MAGIC THE GATHERING</h1>
+      <img className="mountain" src={Mountain} />
+      <img className="swamp" src={Swamp} />
+      <img className="forest" src={Forest} />
+      <img className="island" src={Island} />
+      <img className="plains" src={Plains} />
     </div>
   );
 };
