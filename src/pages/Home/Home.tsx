8be58999ec36a5fdbbda6f023b102a94c82d7e87
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-09-11 16:19:20 +0200

    add hero card

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 // Styles
 import './Home.scss';
 import Mountain from '../../assets/images/Mountain.png';
@@ -7,10 +9,14 @@ import Island from '../../assets/images/Island.png';
 import Plains from '../../assets/images/Plains.png';
 import LogoB from '../../assets/images/LogoB.png';
 import MtgLogo from '../../assets/images/MtgLogo.png';
+import Hero from '../../assets/images/hero2.png';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 const Home = () => {
+  const [heroName, setHeroName] = useState('');
+
   return (
-    <main className="home section-padding">
+    <main className="home">
       <section className="hero">
         <div className="hero-title">
           <h1 className="title">DECKMASTER</h1>
@@ -37,6 +43,73 @@ const Home = () => {
           </div>
           <div className="swamp">
             <img src={Swamp} alt="swamp" />
+          </div>
+        </div>
+        <div className="scroll">
+          <KeyboardDoubleArrowDownIcon />
+          <p>Scroll Down</p>
+          <KeyboardDoubleArrowDownIcon />
+        </div>
+      </section>
+      <section className="begin">
+        <div className="hero-container">
+          <div className="hero-card">
+            <div className="hero-name">
+              <div className="name-border">
+                {heroName ? <h2>{heroName}</h2> : <h2>Unknown Hero</h2>}
+                <div className="mana-cost">
+                  <div className="colorless-mana">
+                    <span>1</span>
+                  </div>
+                  <div className="colorless-mana">
+                    <img src={Swamp} alt="swamp" className="mana" />
+                  </div>
+                  <div className="colorless-mana">
+                    <img src={Swamp} alt="swamp" className="mana" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card-main">
+              <div className="img-container">
+                <img src={Hero} alt="unknown hero" />
+              </div>
+
+              <div className="hero-info">
+                <h4>
+                  This is a blank page, a missing memory fragment that you must
+                  fill in.
+                </h4>
+                <p>more skills</p>
+                <p>more skills</p>
+                <p>more skills</p>
+                <h6>
+                  "Who am'I ? , What is this strange filling ?..... and this
+                  sword"{' '}
+                </h6>
+              </div>
+            </div>
+            <div className="hero-type">
+              <div className="type-border">
+                {heroName ? (
+                  <h3>Legendary Planeswalker - {heroName}</h3>
+                ) : (
+                  <h3>Legendary Planeswalker - Unknown Hero</h3>
+                )}
+              </div>
+              <div className="hero-badge">?</div>
+            </div>
+          </div>
+        </div>
+        <div className="story">
+          <h2>Story...</h2>
+          <p>more story...</p>
+          <div>
+            <input
+              type="text"
+              value={heroName}
+              onChange={(e) => setHeroName(e.target.value)}
+            />
           </div>
         </div>
       </section>
