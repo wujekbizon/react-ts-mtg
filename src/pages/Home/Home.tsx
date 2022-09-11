337bf7a1b3914302c03f07043a514f15584a980c
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-09-11 12:17:45 +0200

    create hero on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,147 +7,39 @@ import Island from '../../assets/images/Island.png';
 import Plains from '../../assets/images/Plains.png';
 import LogoB from '../../assets/images/LogoB.png';
 import MtgLogo from '../../assets/images/MtgLogo.png';
-import AnimatedLetters from '../../components/AnimatedLetters/AnimatedLeters';
-import { useState, useEffect } from 'react';
 
 const Home = () => {
-  // const [letterClass, setLetterClass] = useState('text-animate');
-  const [isActiveForest, setActiveForest] = useState(false);
-  const [isActiveIsland, setActiveIsland] = useState(false);
-  const [isActiveMountain, setActiveMountain] = useState(false);
-  const [isActiveSwamp, setActiveSwamp] = useState(false);
-  const [isActivePlains, setActivePlains] = useState(false);
-  // const nameArray = [
-  //   'M',
-  //   'A',
-  //   'G',
-  //   'I',
-  //   'C',
-  //   ' ',
-  //   'T',
-  //   'H',
-  //   'E',
-  //   ' ',
-  //   'G',
-  //   'A',
-  //   'T',
-  //   'H',
-  //   'E',
-  //   'R',
-  //   'I',
-  //   'N',
-  //   'G',
-  // ];
-
-  // useEffect(() => {
-  //   async function wait() {
-  //     return setTimeout(() => {
-  //       setLetterClass('text-animate-hover');
-  //     }, 4000);
-  //   }
-  //   wait();
-  // }, []);
-
-  const handleToggleForest = () => {
-    setActiveForest(!isActiveForest);
-  };
-
-  const handleToggleIsland = () => {
-    setActiveIsland(!isActiveIsland);
-  };
-
-  const handleToggleMountain = () => {
-    setActiveMountain(!isActiveMountain);
-  };
-
-  const handleToggleSwamp = () => {
-    setActiveSwamp(!isActiveSwamp);
-  };
-
-  const handleTogglePlains = () => {
-    setActivePlains(!isActivePlains);
-  };
-
   return (
-    <main className="home">
-      {/* <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-        </h1>
-      </div> */}
-
-      {/* <div className="container">
-        <div>
-          <img
-            className={`
-          black-logo
-          ${
-            isActiveForest &&
-            isActiveIsland &&
-            isActiveMountain &&
-            isActivePlains &&
-            isActiveSwamp
-              ? 'active-black'
-              : ''
-          }
-          `}
-            src={LogoB}
-            alt="Logo"
-          />
+    <main className="home section-padding">
+      <section className="hero">
+        <div className="hero-title">
+          <h1 className="title">DECKMASTER</h1>
+          <p className="subtitle">
+            Which path will you choose, my Unknown Hero?
+          </p>
+          <div>
+            <p>Made by Fan for Fans</p>
+            <span>Wolfinger Trademark 2022 ⓒ All Rights Reserved</span>
+          </div>
         </div>
-        <div>
-          <img
-            className={`
-          fire-logo
-          ${
-            isActiveForest &&
-            isActiveIsland &&
-            isActiveMountain &&
-            isActivePlains &&
-            isActiveSwamp
-              ? 'active-fire'
-              : ''
-          }
-          `}
-            src={MtgLogo}
-            alt="MTG"
-          />
+        <div className="container">
+          <div className="mountain">
+            <img src={Mountain} alt="moutain" />
+          </div>
+          <div className="forest">
+            <img src={Forest} alt="forest" />
+          </div>
+          <div className="plains">
+            <img src={Plains} alt="plain" />
+          </div>
+          <div className="island">
+            <img src={Island} alt="island" />
+          </div>
+          <div className="swamp">
+            <img src={Swamp} alt="swamp" />
+          </div>
         </div>
-        <div
-          className={`mountain ${isActiveMountain ? 'mountain-animate' : ''} `}
-          onClick={handleToggleMountain}
-        >
-          <img src={Mountain} alt="Mountain" />
-        </div>
-        <div
-          className={`swamp ${isActiveSwamp ? 'swamp-animate' : ''}`}
-          onClick={handleToggleSwamp}
-        >
-          <img src={Swamp} alt="Swamp" />
-        </div>
-        <div
-          className={`forest ${isActiveForest ? 'forest-animate' : ''}`}
-          onClick={handleToggleForest}
-        >
-          <img src={Forest} alt="Forest" />
-        </div>
-        <div
-          className={`island ${isActiveIsland ? 'island-animate' : ''}`}
-          onClick={handleToggleIsland}
-        >
-          <img src={Island} alt="Island" />
-        </div>
-        <div
-          className={`plains ${isActivePlains ? 'plains-animate' : ''}`}
-          onClick={handleTogglePlains}
-        >
-          <img src={Plains} alt="Plains" />
-        </div>
-      </div> */}
+      </section>
     </main>
   );
 };
