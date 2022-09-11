4269c8d3f9881bb2b4775a746d8e4102861e8077
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-09-11 18:51:46 +0200

    add react-parallax

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { Parallax, Background } from 'react-parallax';
 // Styles
 import './Home.scss';
 import Mountain from '../../assets/images/Mountain.png';
@@ -10,6 +10,7 @@ import Plains from '../../assets/images/Plains.png';
 import LogoB from '../../assets/images/LogoB.png';
 import MtgLogo from '../../assets/images/MtgLogo.png';
 import Hero from '../../assets/images/hero2.png';
+import Reborn from '../../assets/images/reborn.jpg';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import { diceRoll } from '../../utils/diceRoll';
 
@@ -59,66 +60,68 @@ const Home = () => {
         </div>
       </section>
       <section className="begin">
-        <div className="hero-container">
-          <div className="hero-card">
-            <div className="hero-name">
-              <div className="name-border">
-                {heroName ? <h2>{heroName}</h2> : <h2>Unknown Hero</h2>}
-                <div className="mana-cost">
-                  <div className="colorless-mana">
-                    <span>1</span>
-                  </div>
-                  <div className="colorless-mana">
-                    <img src={Swamp} alt="swamp" className="mana" />
-                  </div>
-                  <div className="colorless-mana">
-                    <img src={Swamp} alt="swamp" className="mana" />
+        <Parallax className="parallax" bgImage={Reborn} strength={800}>
+          <div className="hero-container">
+            <div className="hero-card">
+              <div className="hero-name">
+                <div className="name-border">
+                  {heroName ? <h2>{heroName}</h2> : <h2>Unknown Hero</h2>}
+                  <div className="mana-cost">
+                    <div className="colorless-mana">
+                      <span>1</span>
+                    </div>
+                    <div className="colorless-mana">
+                      <img src={Swamp} alt="swamp" className="mana" />
+                    </div>
+                    <div className="colorless-mana">
+                      <img src={Swamp} alt="swamp" className="mana" />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="card-main">
-              <div className="img-container">
-                <img src={Hero} alt="unknown hero" />
-              </div>
+              <div className="card-main">
+                <div className="img-container">
+                  <img src={Hero} alt="unknown hero" />
+                </div>
 
-              <div className="hero-info">
-                <h4>
-                  This is a blank page, a missing memory fragment that you must
-                  fill in.
-                </h4>
-                <p>more skills</p>
-                <div className="skill-badge_one">
-                  {roll === 0 ? '?' : '+ 1'}
+                <div className="hero-info">
+                  <h4>
+                    This is a blank page, a missing memory fragment that you
+                    must fill in.
+                  </h4>
+                  <p>more skills</p>
+                  <div className="skill-badge_one">
+                    {roll === 0 ? '?' : '+ 1'}
+                  </div>
+                  <p>more skills</p>
+                  <div className="skill-badge_one two">
+                    {roll === 0 ? '?' : <> -{roll / 3} </>}
+                  </div>
+                  <p>more skills</p>
+                  <div className="skill-badge_one three">
+                    {roll === 0 ? '?' : <> -{roll} </>}
+                  </div>
+                  <h6>
+                    "Who am'I ? , What is this strange filling ?..... and this
+                    sword"{' '}
+                  </h6>
                 </div>
-                <p>more skills</p>
-                <div className="skill-badge_one two">
-                  {roll === 0 ? '?' : <> -{roll / 3} </>}
-                </div>
-                <p>more skills</p>
-                <div className="skill-badge_one three">
-                  {roll === 0 ? '?' : <> -{roll} </>}
-                </div>
-                <h6>
-                  "Who am'I ? , What is this strange filling ?..... and this
-                  sword"{' '}
-                </h6>
               </div>
-            </div>
-            <div className="hero-type">
-              <div className="type-border">
-                {heroName ? (
-                  <h3>Legendary Planeswalker - {heroName}</h3>
-                ) : (
-                  <h3>Legendary Planeswalker - Unknown Hero</h3>
-                )}
-              </div>
-              <div className="hero-badge">
-                {roll === 0 ? '?' : <> {roll} </>}
+              <div className="hero-type">
+                <div className="type-border">
+                  {heroName ? (
+                    <h3>Legendary Planeswalker - {heroName}</h3>
+                  ) : (
+                    <h3>Legendary Planeswalker - Unknown Hero</h3>
+                  )}
+                </div>
+                <div className="hero-badge">
+                  {roll === 0 ? '?' : <> {roll} </>}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Parallax>
         <div className="story">
           <h2>Story...</h2>
           <p>more story...</p>
@@ -139,6 +142,7 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className=""></section>
     </main>
   );
 };
